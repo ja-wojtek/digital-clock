d24716926ff20c6b8ae571561140c519b16628df
Wojtek Mazurek <mazurek.wojciech88@gmail.com>
2023-08-09 07:16:02 +0200

    One second loop

--- js/script.js
+++ js/script.js
@@ -5,17 +5,13 @@ const fieldHour = document.querySelector('.digit-hour');
 const fieldMinute = document.querySelector('.digit-minute');
 const fieldSeconds = document.querySelector('.extra-seconds');
 
-let actualDate = new Date();
-let clockDate = new ClockDate(actualDate);
-
-
-const showTime = () => {
+let showTime = () => {
+  let actualDate = new Date();
+  let clockDate = new ClockDate(actualDate);
   fieldHour.textContent = clockDate.getActualHour();
   fieldMinute.textContent = clockDate.getActualMinutes();
   fieldDay.textContent = clockDate.getActualDay();
   fieldSeconds.textContent = clockDate.getActualSeconds();
 }
 
-showTime();
-
-
+setInterval(showTime, 1000)
